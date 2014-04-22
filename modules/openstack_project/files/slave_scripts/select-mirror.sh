7fa0616201e13e536bbc9e6fbd125c67d9ea90a5
K Jonathan Harker <k.jonathan.harker@hp.com>
2014-04-22 15:16:18 -0700

    Move openstack-specific config out of ::jenkins
    The ::jenkins::slave class contained a lot of openstack-specific
    configuration rather than configuration of a generic jenkins slave.
    
    The term "bare slave" is overloaded and confusing: create simple_slave
    and thick_slave to differentiate between the two meanings of "bare".
    Some portions of ::jenkins::slave will move to simple_slave, some
    portions to thick_slave, and some portions to slave_common (all in the
    openstack_project module).
    
    Change-Id: I5281a03a7f6da3f98714bcc59ae840ace8435578

---
+++ modules/openstack_project/files/slave_scripts/select-mirror.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -x
+
+# Copyright 2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+org=$1
+project=$2
+
+source /usr/local/jenkins/slave_scripts/functions.sh
+check_variable_org_project "$org" "$project" "$0"
+
+rm -f ~/.pydistutils.cfg
+mkdir -p ~/.pip
+rm -f ~/.pip/pip.conf
+
+# Start with a default pip.conf for use with pypi.python.org
+# (which may be overwritten later)
+cat <<EOF > ~/.pip/pip.conf
+[global]
+timeout = 60
+EOF
+
+# For project listed in openstack/requirements,
+# use the pypi.openstack.org mirror exclusively
+if grep -x "$org/$project" /opt/requirements/projects.txt 2>&1
+then
+    export TOX_INDEX_URL='http://pypi.openstack.org/openstack'
+    echo "Switching on internal pypi mirror $TOX_INDEX_URL for $org/$project"
+    cat <<EOF > ~/.pydistutils.cfg
+[easy_install]
+index_url = http://pypi.openstack.org/openstack
+EOF
+    cat <<EOF > ~/.pip/pip.conf
+[global]
+index-url = http://pypi.openstack.org/openstack
+timeout = 60
+EOF
+else
+    echo "$org/$project will not use the internal openstack pypi mirror"
+    cat <<EOF > ~/.pip/pip.conf
+[global]
+timeout = 60
+index-url = http://pypi.openstack.org/openstack
+extra-index-url = http://pypi.python.org/simple
+EOF
+fi
