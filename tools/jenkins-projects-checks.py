9836ea535723f5387b50a97921c75482a77e7110
Andreas Jaeger <aj@suse.de>
2014-10-03 19:51:02 +0200

    Add check for ordering of jenkins/jobs/projects.yaml
    Example output of test:
    $ tools/jenkins-projects-checks.py
    Checking section 'OpenStack server projects'
    Checking section 'OpenStack client projects'
    Checking section 'oslo libraries'
    Checking section 'Other OpenStack projects'
      Wrong alphabetical order: tempest, qa-specs
    Checking section 'OpenStack API projects'
    Checking section 'OpenStack documentation projects'
    Checking section 'OpenStack development projects'
    Checking section 'OpenStack infrastructure projects'
    Checking section 'Stackforge projects'
    Found errors in jenkins/jobs/projects.yaml!
    
    This patch fixes the order so that it passes.
    It also creates a new job to test this.
    
    Change-Id: I36fd522261363494f07b57e40f0d9b943cf08841

---
+++ tools/jenkins-projects-checks.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+# Copyright 2014 SUSE Linux Products GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+
+def normalize(s):
+    "Normalize string for comparison."
+    return s.lower().replace("_", "-")
+
+
+def check_sections():
+    """Check that the projects are in alphabetical order per section."""
+
+    # Note that the file has different sections and we need to check
+    # entries within these sections only
+    errors = False
+    last = ""
+    for line in open('jenkins/jobs/projects.yaml', 'r'):
+        if line.startswith('# Section:'):
+            last = ""
+            section = line[10:].strip()
+            print("Checking section '%s'" % section)
+        if ' name: ' in line:
+            i = line.find(' name: ')
+            current = line[i + 7:].strip()
+            if normalize(last) > normalize(current):
+                print("  Wrong alphabetical order: %(last)s, %(current)s" %
+                      {"last": last, "current": current})
+                errors = True
+            last = current
+    return errors
+
+
+def check_all():
+    errors = check_sections()
+
+    if errors:
+        print("Found errors in jenkins/jobs/projects.yaml!")
+    else:
+        print("No errors found in jenkins/jobs/projects.yaml!")
+    return errors
+
+if __name__ == "__main__":
+    sys.exit(check_all())
