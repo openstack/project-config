e2bf99eb12324e9db10d03c80c98a3eeb80c9cea
Ian Wienand <iwienand@redhat.com>
2014-10-29 16:06:17 +1100

    Use full path to grub2-mkconfig
    Current centos7 builds on hpcloud are failing calling grub2-mkconfig;
    from the logs:
    
     + sudo grub2-mkconfig -o /boot/grub2/grub.cfg
     sudo: grub2-mkconfig: command not found
    
    To be completely honest, I'm having a hard time explaining why this
    works with rackspace images, but not hpcloud ones.  I can not
    replicate this in my own setup, even using the same hpcloud images.
    Possibly it's something to do with the nodepool host environment when
    it starts the script on the remote host?  Without access to a failing
    host I'm clutching at straws...
    
    Change-Id: I9d9f759fac1a96cc785354ae5aa44398c908704d

--- nodepool/scripts/restrict_memory.sh
+++ nodepool/scripts/restrict_memory.sh
@@ -24,7 +24,7 @@ if [ -f /etc/default/grub ] ; then
         sudo update-grub
     else
         # If update-grub isn't available, use grub2-mkconfig directly
-        sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+        sudo /usr/sbin/grub2-mkconfig -o /boot/grub2/grub.cfg
     fi
 elif [ -f /boot/grub/grub.conf ] ; then
     sudo sed -i -e 's/^timeout=[0-9]\+/timeout=0/' -e 's/\(^\s\+kernel.*\)/\1 mem=8G/' /boot/grub/grub.conf
