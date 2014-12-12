353a5e324b09738f225f74e50d89d6cf343a9d79
Joshua Hesketh <josh@nitrotech.org>
2014-12-12 14:54:41 +1100

    zuul's log path should always be a folder
    zuul sets the path for where logs are stored. In all our cases this
    is a folder containing the console.log and other assets. We should
    denote this is a folder by leaving a trailing slash. This also means
    that os-loganalyze can determine if it should load an index.html
    faster by looking for a trailing slash.
    
    Change-Id: Ia1de627c01ee43c461d25b184fb61cbfd91fb19d

--- zuul/openstack_functions.py
+++ zuul/openstack_functions.py
@@ -17,18 +17,18 @@ import re
 
 def set_log_url(item, job, params):
     if hasattr(item.change, 'refspec'):
-        path = "%s/%s/%s/%s" % (
+        path = "%s/%s/%s/%s/" % (
             params['ZUUL_CHANGE'][-2:], params['ZUUL_CHANGE'],
             params['ZUUL_PATCHSET'], params['ZUUL_PIPELINE'])
     elif hasattr(item.change, 'ref'):
-        path = "%s/%s/%s" % (
+        path = "%s/%s/%s/" % (
             params['ZUUL_NEWREV'][:2], params['ZUUL_NEWREV'],
             params['ZUUL_PIPELINE'])
     else:
         path = params['ZUUL_PIPELINE']
     params['BASE_LOG_PATH'] = path
-    params['LOG_PATH'] = path + '/%s/%s' % (job.name,
-                                            params['ZUUL_UUID'][:7])
+    params['LOG_PATH'] = path + '/%s/%s/' % (job.name,
+                                             params['ZUUL_UUID'][:7])
 
 
 def reusable_node(item, job, params):
