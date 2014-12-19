a3034dfcbea2654141a527a34a6a31e97d2aca6f
armando-migliaccio <armamig@gmail.com>
2014-12-19 11:34:50 -0800

    Add openstack-infra to the list of namespaces to check.
    Change-Id: I8ff59dccc06df7ab23d13eca6990418f3a547d34

--- tools/check_valid_gerrit_config.sh
+++ tools/check_valid_gerrit_config.sh
@@ -25,7 +25,7 @@ function check_team_acl {
 }
 
 # Add more namespaces here, if necessary
-for namespace in stackforge openstack openstack-dev; do
+for namespace in openstack openstack-dev openstack-infra stackforge; do
     check_team_acl "${CONFIGS_LIST_BASE}${namespace}"
 done
 
