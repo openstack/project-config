f8788e2e275c0aa1e2b08681b4eaaefbe13b6e33
Andreas Jaeger <aj@suse.de>
2015-03-06 10:39:45 +0100

    Ensure that gerritbot channels are in accessbot
    Add new test that checks that channels mentioned in gerritbot are also
    in accessbot.
    
    Enhance the irc tox target so that it runs the checks, and update Zuul
    so that the job is run for changes to gerritbot.
    
    Add channels openstack-mistral and puppet-openstack to accessbot,
    openstackinfra now has access to the channels. These two channels are
    already in gerritbot.
    
    Make check_irc_access.py script executable.
    
    Change-Id: Ic68ba9a1c60854e68817fcadb1a5df2144944af7

---
+++ tools/irc_tests.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+# Copyright 2015 SUSE Linux GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import yaml
+
+
+def access_gerrit_check():
+    "Check that all channels in gerritbot are in accessbot as well."
+
+    errors = False
+
+    access_config = yaml.load(open('accessbot/channels.yaml', 'r'))
+
+    access_channels = []
+    for channel in access_config['channels']:
+        access_channels.append(channel['name'])
+
+    access_channel_set = set(access_channels)
+
+    gerrit_config = yaml.load(open('gerritbot/channels.yaml'))
+
+    print("Checking that all channels in gerritbot are also in accessbot")
+    for channel in gerrit_config:
+        if channel not in access_channel_set:
+            print("  %s is missing from accessbot" % channel)
+            errors = True
+
+    return errors
+
+
+def main():
+    errors = access_gerrit_check()
+
+    if errors:
+        print("Found errors in channel setup!")
+    else:
+        print("No errors found in channel setup!")
+    return errors
+
+if __name__ == "__main__":
+    main()
