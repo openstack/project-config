10e728cfed0ea68c8e40f5d9c30939969f7edaf7
Jeremy Stanley <fungi@yuggoth.org>
2015-04-30 17:14:10 +0000

    Merge tags from stable branches to master
    We no longer use proposed branches, so should merge tags from stable
    branches to master but only when appropriate.
    
    Change-Id: I6e440586d6bdf0e4f7f1fa62f25c4a77414950c7

--- jenkins/scripts/merge_tags.sh
+++ jenkins/scripts/merge_tags.sh
@@ -14,20 +14,43 @@
 
 TAG=$1
 
-# Only merge release tag if it's not on a stable branch
-if $(git branch -r --contains "$TAG" | grep "stable/" >/dev/null)
-then
-    echo "Tag $TAG was pushed to a stable branch, ignoring."
-else
-    git config user.name "OpenStack Proposal Bot"
-    git config user.email "openstack-infra@lists.openstack.org"
-    git config gitreview.username "proposal-bot"
-
-    git review -s
-    git checkout master
-    git reset --hard origin/master
-    git merge --no-edit -s ours $TAG
-    # Get a Change-Id
-    GIT_EDITOR=true git commit --amend
-    git review -R -y -t merge/release-tag
+# Avoid acting on any tag which is not strictly an all-numeric/stops string
+# since the logic below relies on a simple numeric sort. Ordering of
+# non-numeric strings are locale-specific, and situations like PEP-440 violate
+# assumptions about non-numeric versions being alpha-sorted anyway. This check
+# is redundant since we should never allow a tag containing non-numeric version
+# components through the filter for the release pipeline in Zuul, but is here
+# as a safeguard to validate that expectation since "Git tags are forever."
+if ! echo $TAG|grep '^[0-9\.]*$'; then
+    echo "ERROR: Triggered on non-release $TAG tag."
+    exit 1
 fi
+
+git config user.name "OpenStack Proposal Bot"
+git config user.email "openstack-infra@lists.openstack.org"
+git config gitreview.username "proposal-bot"
+
+git review -s
+git remote update
+git checkout master
+git reset --hard origin/master
+MASTER_MINOR=`git describe|cut -d. -f-2`
+TAG_MINOR=`echo $TAG|cut -d. -f-2`
+
+# If the tag is for a patch version where its minor matches master's, skip
+if [ "$MASTER_MINOR" = "$TAG_MINOR" ]; then
+    echo "Skipping $TAG which shares master's $MASTER_MINOR version."
+    exit 0
+fi
+
+# If the tag is for an earlier version than master's, skip
+if [ "$(echo $(echo -e "$MASTER_MINOR\n$TAG_MINOR"|sort -V))" \
+    = "$TAG_MINOR $MASTER_MINOR" ]; then
+    echo "Skipping $TAG which sorts before master's $MASTER_MINOR version."
+    exit 0
+fi
+
+git merge --no-edit -s ours $TAG
+# Get a Change-Id
+GIT_EDITOR=true git commit --amend
+git review -R -y -t merge/release-tag
