75977ad1de0a7c5ed0aa3aece3eec7e286f7cf24
Steve Kowalik <steven@wedontsleep.org>
2015-06-24 16:10:36 +1000

    Add a script to populate a Zanata project
    Currently, the Zanata server has all of the OpenStack projects
    configured, but there are no source documents and no translations
    for them. Concoct a script that will pull all translations from
    Transifex and then push them up with current source documents to
    our Zanata server.
    
    Change-Id: Ic5c28f4ed238314d82919a7e6eb8fe64b804a220

---
+++ jenkins/scripts/populate-zanata-project.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -xe
+
+# Copyright (c) 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+PROJECT=$1
+
+source /usr/local/jenkins/slave_scripts/common_translation_update.sh
+
+setup_translation
+case "$PROJECT" in
+    api-site|ha-guide|operations-guide|security-doc)
+        init_manuals "$PROJECT"
+        setup_manuals "$PROJECT"
+        ;;
+    django-openstack-auth)
+        setup_django_openstack_auth
+        ;;
+    horizon)
+        setup_horizon
+        ;;
+    *)
+        setup_project "$PROJECT"
+        ;;
+esac
+
+# Download all files from transifex
+tx pull -a -f --minimum-perc=0
+
+# And upload them to Zanata
+zanata-cli -B -e push --push-type both
+
+# Undo everything we did locally
+git reset --hard
