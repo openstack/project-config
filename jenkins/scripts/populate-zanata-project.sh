218ebf1080aaa0acc52a26bd5f068676c01243fa
Steve Kowalik <steven@wedontsleep.org>
2015-07-08 13:51:55 +1000

    Add openstack-manuals to populate-zanata-project
    It turns out that openstack-manuals also contains documents for
    translations, so treat it like one.
    
    Change-Id: I52f3a322131f8fde46ff6c326887d680576ff0ae

--- jenkins/scripts/populate-zanata-project.sh
+++ jenkins/scripts/populate-zanata-project.sh
@@ -20,7 +20,7 @@ source /usr/local/jenkins/slave_scripts/common_translation_update.sh
 
 setup_translation
 case "$PROJECT" in
-    api-site|ha-guide|operations-guide|security-doc)
+    api-site|ha-guide|openstack-manuals|operations-guide|security-doc)
         init_manuals "$PROJECT"
         setup_manuals "$PROJECT"
         ;;
