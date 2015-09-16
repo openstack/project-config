28eb4ce6f5ac6280ec5a61c13992e5a7b8b1026b
Steve Kowalik <steven@wedontsleep.org>
2015-09-16 15:53:14 +1000

    Remove support for pulling from Transifex
    Switch the translation scripts to only pull from Zanata, no matter
    what extra arguments they are passed on the command line. This does
    not completly remove support for Transifex, which will be cleaned up
    in a future commit.
    
    Change-Id: I21bd54d4205693315da1152a310ff4faf22531a5

--- jenkins/scripts/propose_translation_update_manuals.sh
+++ jenkins/scripts/propose_translation_update_manuals.sh
@@ -18,41 +18,19 @@
 # and push to Gerrit.
 
 PROJECT=$1
-SOFTWARE="Transifex"
-
-if [ -n "$2" -a "$2" = "zanata" ]; then
-    SOFTWARE="Zanata"
-fi
 
 source /usr/local/jenkins/slave_scripts/common_translation_update.sh
 
 init_manuals "$PROJECT"
 
 setup_git
-setup_review "$SOFTWARE"
+setup_review
 setup_translation
 
 setup_manuals "$PROJECT"
 
-# Pull updated translations from Transifex, or Zanata.
-case "$SOFTWARE" in
-    Transifex)
-        # Download new files.
-        # Also downloads updates for existing files that are
-        # translated to a certain amount as configured in setup_manuals.
-        # The function setup_manuals will setup most files --minimum-perc=75
-        # for most files.
-        tx pull -a -f
-
-        # Pull upstream translations of all downloaded files but do not
-        # download new files.
-        # Use lower percentage here to update the existing files.
-        tx pull -f --minimum-perc=50
-        ;;
-    Zanata)
-        pull_from_zanata_manuals "$PROJECT"
-        ;;
-esac
+# Pull updated translations from Zanata.
+pull_from_zanata_manuals "$PROJECT"
 
 # Compress downloaded po files
 # Only touch glossary in openstack-manuals but not in any other
