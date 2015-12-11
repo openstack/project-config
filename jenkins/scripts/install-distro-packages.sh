093a68eef307e2341fb263157630e95d382a3b99
Jeremy Stanley <fungi@yuggoth.org>
2015-12-11 19:07:46 +0000

    Make install-distro-packages.sh configurable
    Allow the calling environment to override the default paths to the
    bindep entrypoint and to the packages list.
    
    Change-Id: I4f305b454e4f88365085bdb5a4a079fcf282ff45

--- jenkins/scripts/install-distro-packages.sh
+++ jenkins/scripts/install-distro-packages.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -xe
 
+# set a default path to the preinstalled bindep entrypoint
+export BINDEP=${BINDEP:-/usr/bindep-env/bin/bindep}
+
 # figure out which bindep list to use
-if [ -e other-requirements.txt ] ; then
+if [ -n "$PACKAGES" ] ; then
+    # already set in the calling environment
+elif [ -e other-requirements.txt ] ; then
     # project has its own bindep list
-    export DISTRO_PACKAGES=other-requirements.txt
-elif [ "$ZUUL_PROJECT" == "openstack-infra/project-config" ] ; then
-    # test changes to the included fallback list in project-config jobs
-    export DISTRO_PACKAGES=jenkins/data/bindep-fallback.txt
+    export PACKAGES=other-requirements.txt
 else
     # use the bindep fallback list preinstalled on the worker
-    export DISTRO_PACKAGES=/usr/local/jenkins/common_data/bindep-fallback.txt
+    export PACKAGES=/usr/local/jenkins/common_data/bindep-fallback.txt
 fi
 
 # install all requested packages from the appropriate bindep list
@@ -17,9 +19,8 @@ if apt-get -v >/dev/null ; then
     sudo apt-get update
     sudo PATH=/usr/sbin:/sbin:$PATH DEBIAN_FRONTEND=noninteractive \
         apt-get --option "Dpkg::Options::=--force-confold" \
-        --assume-yes install \
-        `/usr/bindep-env/bin/bindep -b -f $DISTRO_PACKAGES || true`
+        --assume-yes install `$BINDEP -b -f $PACKAGES || true`
 else
     sudo PATH=/usr/sbin:/sbin:$PATH yum install -y \
-        `/usr/bindep-env/bin/bindep -b -f $DISTRO_PACKAGES || true`
+        `$BINDEP -b -f $PACKAGES || true`
 fi
