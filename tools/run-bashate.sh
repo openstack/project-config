0e327b9092dd36a650561d7844e019ca3cb2d8c1
James E. Blair <jeblair@linux.vnet.ibm.com>
2016-01-07 16:51:45 -0800

    Ignore E011 in bashate
    Change-Id: Id735a21cda1fc46e40b4f9ea51410d89bbf000a1

--- tools/run-bashate.sh
+++ tools/run-bashate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# E011 is intentionally ignored because it does not make sense; it is
+# perfectly reasonable to put 'then' on a separate line when natural
+# line breaks occur in long conditionals.
+
 ROOT=$(readlink -fn $(dirname $0)/.. )
 find $ROOT -not -wholename \*.tox/\* -and -not -wholename \*.test/\* \
-    -and -name \*.sh -print0 | xargs -0 bashate -v --ignore E006
+    -and -name \*.sh -print0 | xargs -0 bashate -v --ignore E006,E011
