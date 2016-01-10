82c56471fe9741c595a0138fd454754194180475
Steve Martinelli <stevemar@ca.ibm.com>
2016-01-10 02:25:15 -0500

    add ironic-inspector and tripleo to osc-check-plugins job
    python-ironic-inspector-client and python-tripleoclient also use
    OSC, and should be included in the install script and the layout.
    
    Change-Id: I675d57e7bbe218fae2bc8215ae2dac4cb233909d

--- jenkins/scripts/check-osc-plugins.sh
+++ jenkins/scripts/check-osc-plugins.sh
@@ -41,8 +41,10 @@ install_from_source python-cueclient
 install_from_source python-designateclient
 install_from_source python-heatclient
 install_from_source python-ironicclient
+install_from_source python-ironic-inspector-client
 install_from_source python-mistralclient
 install_from_source python-saharaclient
+install_from_source python-tripleoclient
 install_from_source python-tuskarclient
 install_from_source python-zaqarclient
 
