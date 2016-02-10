b51894c602db8e533f3ebecca429a15fd096db2e
Brant Knudson <bknudson@us.ibm.com>
2016-02-10 19:59:07 +0000

    Revert "Support stable requirements"
    This reverts commit c01b96e6da166032ef13aa7ca057ab81bcc23391.
    
    Rather than change project-requirements-change.py to support
    old requirements, we'll backport changes to requirements.
    
    Change-Id: Id6129e19988cad882629cff6c251bbaa81997d5c

--- jenkins/scripts/project-requirements-change.py
+++ jenkins/scripts/project-requirements-change.py
@@ -228,7 +228,7 @@ def main():
                 if reqs == global_reqs[name]:
                     continue
                 for req in reqs:
-                    if hasattr(req, 'extras') and req.extras:
+                    if req.extras:
                         for extra in req.extras:
                             counts[extra] = counts.get(extra, 0) + 1
                     else:
