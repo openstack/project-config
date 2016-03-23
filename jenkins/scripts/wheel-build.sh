bba0d90ab7a7a98e4eea7825c5e228fd2be93b9a
Ian Wienand <iwienand@redhat.com>
2016-03-23 11:45:15 +1100

    Add verbose to wheel build
    This helps debug what might be missing when a wheel build fails
    
    Change-Id: I775bb3eb577b104002d045bc337f20e72a03adff

--- jenkins/scripts/wheel-build.sh
+++ jenkins/scripts/wheel-build.sh
@@ -10,5 +10,5 @@ BRANCHES=`git --git-dir=$WORKING_DIR/.git branch -r | grep '^  origin/[^H]'`
 for BRANCH in $BRANCHES; do
     git --git-dir=$WORKING_DIR/.git show $BRANCH:upper-constraints.txt \
         2>/dev/null > /tmp/upper-constraints.txt  || true
-    pip wheel -r /tmp/upper-constraints.txt -w $WHEELHOUSE_DIR || true
+    pip --verbose wheel -r /tmp/upper-constraints.txt -w $WHEELHOUSE_DIR || true
 done
