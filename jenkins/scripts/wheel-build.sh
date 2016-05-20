8cbb58577797df8a821c804657a705e1e856a5eb
Victor Stinner <vstinner@redhat.com>
2016-05-20 14:45:24 +0200

    Fix wheel-build.sh for Python 3
    Replace pip3 command with "python3 -m pip": the pip3 command doesn't exist on
    the builder. Moreover, "python -m pip" is now recommended over "pip".
    
    Replace also the pip command with "python -m pip".
    
    Change-Id: Idbc6e53bf2f54a9da952fd940efb8e41e6b7c253
    Related-Bug: 1582257

--- jenkins/scripts/wheel-build.sh
+++ jenkins/scripts/wheel-build.sh
@@ -10,6 +10,6 @@ BRANCHES=`git --git-dir=$WORKING_DIR/.git branch -r | grep '^  origin/[^H]'`
 for BRANCH in $BRANCHES; do
     git --git-dir=$WORKING_DIR/.git show $BRANCH:upper-constraints.txt \
         2>/dev/null > /tmp/upper-constraints.txt  || true
-    pip --verbose wheel -r /tmp/upper-constraints.txt -w $WHEELHOUSE_DIR || true
-    pip3 --verbose wheel -r /tmp/upper-constraints.txt -w $WHEELHOUSE_DIR || true
+    python -m pip --verbose wheel -r /tmp/upper-constraints.txt -w $WHEELHOUSE_DIR || true
+    python3 -m pip --verbose wheel -r /tmp/upper-constraints.txt -w $WHEELHOUSE_DIR || true
 done
