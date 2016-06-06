928a901dab48b677ca61564e52a00725620d61fe
Jeremy Stanley <fungi@yuggoth.org>
2016-06-06 17:39:54 +0000

    Revert "Retry twine uploads in a loop"
    This reverts commit 9ee3e76a66a9f5fd535de8031a7585dff6ea6f77.
    
    Revert "Check twine uploads for success"
    
    This reverts commit 16b12ba5b95e9696fa58719d671e9fe85e400ea8.
    
    With the move to warehouse uploads in
    Ib9d3d2bd02358947433aeb9520eb1bc1088392eb we no longer need these
    check and retry workarounds for long-standing PyPI uploading
    misbehavior. Also, per IRC discussion with Donald Stufft, those URLs
    likely never worked the way we thought anyway.
    
    Change-Id: I71ffcc0afadadb6c7233d49ea7941989b1f690ef

--- jenkins/scripts/pypi-tarball-upload.sh
+++ jenkins/scripts/pypi-tarball-upload.sh
@@ -33,22 +33,4 @@ curl --fail -o $FILENAME http://$TARBALL_SITE/$PROJECT/$FILENAME
 # Make sure we actually got a gzipped file
 file -b $FILENAME | grep gzip
 
-# Uploads may claim to fail but actually succeed so we check if we
-# can download after upload to determine success. They can also fail
-# intermittently, so retrying in a delayed loop helps improve
-# robustness.
-TRY=0
-RETVAL=255
-set +e
-while [[ $TRY -lt 3 ]] && [[ $RETVAL -ne 0 ]]; do
-    twine upload -r pypi $FILENAME
-    curl --head --silent --fail \
-        "https://pypi.python.org/simple/$PROJECT/$FILENAME" >/dev/null 2>&1
-    RETVAL=$?
-    (( TRY++ ))
-    if [[ $TRY -lt 3 ]] && [[ $RETVAL -ne 0 ]]; then
-        echo "Upload failed, retrying in $TRY seconds." >&2
-        sleep $TRY
-    fi
-done
-exit $RETVAL
+twine upload -r pypi $FILENAME
