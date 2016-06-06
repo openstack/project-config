ebf1ff3fba817ff85cb90605f040808e024f12cd
Andreas Jaeger <aj@suse.com>
2016-06-06 18:51:29 +0200

    Add infra structure for install guide publishing
    Add job template and zuul template for install guides. Use
    jobs for heat.
    
    This publishes content from $repo/install-guide to
    docs.openstack.org/project-install-guide/SERVICE/BRANCH.
    
    The jobs should only run on future branches (master and newton onwards).
    
    Depends-On: I153b27dd17e5a22224bda473b19dbd76ca0cdc98
    Change-Id: Id3ae8e13331f54886c994731c1770af1bdcfb3a2
    Partially-Implements: blueprint projectspecificinstallguides

---
+++ jenkins/scripts/run-install-guide.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -xe
+
+# This script is used to publish project-specific install-guide
+# documents to the proper place. Master will be published to a draft
+# directory, stable/X will be published to the X directory. For
+# example stable/newton documents will life in the newton directory.
+
+tox -e install-guide
+result=$?
+
+[ -e .tox/$venv/bin/pbr ] && freezecmd=pbr || freezecmd=pip
+
+echo "Begin pbr freeze output from test virtualenv:"
+echo "======================================================================"
+.tox/${venv}/bin/${freezecmd} freeze
+echo "======================================================================"
+
+if [ -z "$ZUUL_REFNAME" ]; then
+    TARGET=""
+    # Leave documents where they are
+elif [ "$ZUUL_REFNAME" == "master" ] ; then
+    TARGET=draft
+elif echo $ZUUL_REFNAME | grep stable/ >/dev/null ; then
+    # Put stable release changes in dir named after stable release under the
+    # build dir. When Jenkins copies these files they will be accessible under
+    # the developer docs root using the stable release's name.
+    TARGET=$(echo $ZUUL_REFNAME | sed 's/stable.//')
+else
+    # Put other branch changes in dir named after branch under the
+    # build dir. When Jenkins copies these files they will be
+    # accessible under the developer docs root using the branch name.
+    # EG: feature/foo or milestone-proposed
+    TARGET=$ZUUL_REFNAME
+fi
+
+if [ ! -z $TARGET ] ; then
+    # Move the docs into subdir based on branch
+    mkdir install-guide/build/$TARGET
+    mv doc/build/html/* doc/build/$TARGET
+    mv doc/build/$TARGET doc/build/html/$TARGET
+fi
+
+exit $result
