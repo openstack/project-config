f9acae2df9d9a7a4c6f25eece6b016fe5106e8ce
Paul Belanger <pabelanger@redhat.com>
2016-06-13 10:46:11 -0400

    Add python version to wheel builders
    This should allow us to break our our different python version into
    different job runs.
    
    Change-Id: I8e3ed20cf269a12fa06be657301356334953331a
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- jenkins/scripts/wheel-build.sh
+++ jenkins/scripts/wheel-build.sh
@@ -4,6 +4,7 @@
 WHEELHOUSE_DIR=$1
 PROJECT=openstack/requirements
 WORKING_DIR=`pwd`/$PROJECT
+PYTHON_VERSION=$2
 
 # Extract and iterate over all the branch names.
 BRANCHES=`git --git-dir=$WORKING_DIR/.git branch -r | grep '^  origin/[^H]'`
@@ -11,6 +12,6 @@ for BRANCH in $BRANCHES; do
     git --git-dir=$WORKING_DIR/.git show $BRANCH:upper-constraints.txt \
         2>/dev/null > /tmp/upper-constraints.txt  || true
     rm -rf build_env
-    virtualenv build_env
+    virtualenv -p $PYTHON_VERSION $build_env
     build_env/bin/pip --verbose wheel -r /tmp/upper-constraints.txt -w $WHEELHOUSE_DIR || true
 done
