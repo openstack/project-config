a31c54473c8c00a397a137094e6d1f814161b46c
Doug Hellmann <doug@doughellmann.com>
2016-06-21 15:31:31 -0400

    import the release tools that need to run on secure nodes
    Move the scripts that need to run on secure nodes to automatically tag
    releases from release-tools to this repo.
    
    Change-Id: I66e15cd0fa270e9b7a5cb3849cb8d14fa4c617b1
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ jenkins/scripts/release-tools/branch_from_yaml.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Script to create stable branches based on changes to the
+# deliverables files in the openstack/releases repository.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+set -e
+
+TOOLSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $TOOLSDIR/functions
+
+function usage {
+    echo "Usage: branch_from_yaml.sh releases_repository series [deliverable_files]"
+    echo
+    echo "Example: branch_from_yaml.sh ~/repos/openstack/releases mitaka"
+    echo "Example: branch_from_yaml.sh ~/repos/openstack/releases mitaka"
+    echo "Example: branch_from_yaml.sh ~/repos/openstack/releases mitaka deliverables/mitaka/oslo.config.yaml"
+}
+
+if [ $# -lt 2 ]; then
+    echo "ERROR: Please specify releases_repository and series"
+    echo
+    usage
+    exit 1
+fi
+
+RELEASES_REPO="$1"
+shift
+SERIES="$1"
+shift
+DELIVERABLES="$@"
+
+$TOOLSDIR/list_deliverable_changes.py -r $RELEASES_REPO $DELIVERABLES \
+| while read deliverable ignore_series version repo ignore_hash ignore_announce_to pypi ignore_first_full; do
+    title "$repo $series $version"
+    $TOOLSDIR/make_stable_branch.sh $SERIES $repo $version
+done
+
+exit 0
---
+++ jenkins/scripts/release-tools/release_from_yaml.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# Script to release projects based on changes to the deliverables
+# files in the openstack/releases repository.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+set -e
+
+TOOLSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $TOOLSDIR/functions
+
+function usage {
+    echo "Usage: release_from_yaml.sh releases_repository [deliverable_files]"
+    echo
+    echo "Example: release_from_yaml.sh ~/repos/openstack/releases"
+    echo "Example: release_from_yaml.sh ~/repos/openstack/releases"
+    echo "Example: release_from_yaml.sh ~/repos/openstack/releases deliverables/mitaka/oslo.config.yaml"
+}
+
+if [ $# -lt 1 ]; then
+    echo "ERROR: No releases_repository specified"
+    echo
+    usage
+    exit 1
+fi
+
+RELEASES_REPO="$1"
+shift
+DELIVERABLES="$@"
+
+# Configure git to pull the notes where gerrit stores review history
+# like who approved a patch.
+cd $RELEASES_REPO
+if ! git config --get remote.origin.fetch | grep -q refs/notes/review; then
+    git config --add remote.origin.fetch refs/notes/review:refs/notes/review
+    git config --add core.notesRef refs/notes/review
+    git remote update origin
+fi
+
+# Look for metadata about the release instructions to include in the
+# tag message.
+if git show --no-patch --pretty=format:%P | grep -q ' '; then
+    # multiple parents, look at the submitted patch instead of the
+    # merge commit
+    parent=$(git show --no-patch --pretty=format:%P | cut -f2 -d' ')
+else
+    # single parent, look at the current patch
+    parent=''
+fi
+RELEASE_META=$(git show --format=full --show-notes=review $parent | egrep -i '(Author|Commit:|Code-Review|Workflow|Change-Id)' | sed -e 's/^    //g' -e 's/^/meta:release:/g')
+
+$TOOLSDIR/list_deliverable_changes.py -r $RELEASES_REPO $DELIVERABLES \
+| while read deliverable series version repo hash announce_to pypi first_full; do
+    title "$repo $series $version $hash $announce_to"
+    $TOOLSDIR/release.sh $repo $series $version $hash $announce_to $pypi $first_full "$RELEASE_META"
+done
+
+exit 0
