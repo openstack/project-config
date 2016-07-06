f4b4706d831343a35c2a8b8506a9b1dd91c3ae7f
Armando Migliaccio <armamig@gmail.com>
2016-07-06 14:54:51 -0700

    Add job configuration for unit testing against neutron-lib source
    Testing neutron-lib periodically with unit test jobs from  Neutron stadium
    projects can help prevent breakages detected at the time of a neutron-lib
    release and it will speed development and maturity of the library.
    
    This patch introduces the configuration necessary for running unit
    tests against the master version of neutron-lib. The support is limited
    to py34, as that would suffice to ensure coverage.
    
    Change-Id: Ib1e4dbf92aab4ec09ad3ce54255ca729941784fa

---
+++ jenkins/scripts/run-tox-with-neutron-lib-master.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -xe
+
+# inspired from sister script run-tox-with-oslo-master.sh
+
+venv=$1
+
+if [[ -z "$venv" ]]; then
+    echo "Usage: $?"
+    echo
+    echo "VENV: The tox environment to run (eg 'py34')"
+    exit 1
+fi
+
+script_path=/usr/local/jenkins/slave_scripts
+
+sed -i "s/neutron-lib.*/-e git+https:\/\/git.openstack.org\/openstack\/neutron-lib.git#egg=neutron-lib/g" requirements.txt
+
+cat << EOF >> tox.ini
+
+[testenv:py34-neutron-lib-master]
+setenv = VIRTUAL_ENV={envdir}
+passenv = TRACE_FAILONLY GENERATE_HASHES http_proxy HTTP_PROXY https_proxy HTTPS_PROXY no_proxy NO_PROXY
+usedevelop = True
+deps = -r{toxinidir}/requirements.txt
+       -r{toxinidir}/test-requirements.txt
+whitelist_externals = sh
+commands =
+  {toxinidir}/tools/ostestr_compat_shim.sh {posargs}
+EOF
+
+$script_path/run-tox.sh $venv-neutron-lib-master
