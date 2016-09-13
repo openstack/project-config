09f6cb1c39646d0fd8d6918595bead8be9d73a15
Sean Dague <sean@dague.net>
2016-09-13 07:31:35 -0400

    Cross test with zuul api-ref on projects
    This allows using the zuul api-ref reference on a project in question,
    it will let us see what api-ref changes look like on existing api-ref
    source trees.
    
    Change-Id: I263278370d93cd4998ee3da518a5b0de6bd80548

---
+++ jenkins/scripts/run-tox-api-ref.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -xe
+
+# If a bundle file is present, call tox with the jenkins version of
+# the test environment so it is used.  Otherwise, use the normal
+# (non-bundle) test environment.  Also, run pbr freeze on the
+# resulting environment at the end so that we have a record of exactly
+# what packages we ended up testing.
+#
+# Usage: run-tox-api-ref.sh API_REF_DIR
+#
+# Where VENV is the name of the tox environment to run (specified in the
+# project's tox.ini file).
+
+api_ref_dir=${1:-os-api-ref}
+
+script_path=/usr/local/jenkins/slave_scripts
+
+cat <<EOF >> tox.ini
+[testenv:api-ref-src]
+commands =
+    pip install -q -U -e $api_ref_dir
+    sphinx-build -W -b html -d api-ref/build/doctrees api-ref/source api-ref/build/html
+EOF
+
+$script_path/run-tox.sh api-ref-src
