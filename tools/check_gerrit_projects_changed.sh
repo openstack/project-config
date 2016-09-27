f2c6295f25d709f39ebccf5f01ba6d1a1e72749f
Andreas Jaeger <aj@suse.com>
2016-09-27 21:45:39 +0200

    Check changed track-upstreams in gerrit/projects.yaml
    Check changes to the upstream URL for a repo that has option
    track-upstream. The check currently is just cloning of the repository.
    
    Since we currently have over 580 repos with track upstream, this check
    compares the previous version of the file with the current one and only
    tests for changed entries.
    
    Change-Id: Ic716c685512ebe660876388d663314bf3b008b66

---
+++ tools/check_gerrit_projects_changed.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+
+GITHEAD=$(git rev-parse HEAD)
+
+# Check out previous version
+git checkout HEAD~1
+
+cp gerrit/projects.yaml gerrit/projects-old.yaml
+
+# Back to current version. Otherwise the
+# check_gerrit_projects_changed.py invocation might be an old version.
+git checkout $GITHEAD
+
+python tools/check_gerrit_projects_changed.py gerrit/projects-old.yaml \
+    gerrit/projects.yaml
