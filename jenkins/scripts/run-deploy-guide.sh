48c0665f5df978c133f553c9c8120fef5a20f080
Alexandra Settle <alexandra.settle@rackspace.com>
2016-11-10 14:42:42 -0500

    Add deployment guide script
    Add script to build the deployment guide. This will be used by a
    followup change.
    
    Change-Id: I0cdc12e894f3ac3c75ff84ed2ecd634b2667e18f

---
+++ jenkins/scripts/run-deploy-guide.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -xe
+
+# This script is used to publish project-specific deploy-guide
+# documents to the proper place. Master will be published to a draft
+# directory, stable/X will be published to the X directory. For
+# example stable/newton documents will life in the newton directory.
+
+venv=deploy-guide
+
+export UPPER_CONSTRAINTS_FILE=$(pwd)/upper-constraints.txt
+
+tox -e $venv
+result=$?
+
+[ -e .tox/$venv/bin/pbr ] && freezecmd=pbr || freezecmd=pip
+
+echo "Begin pbr freeze output from test virtualenv:"
+echo "======================================================================"
+.tox/${venv}/bin/${freezecmd} freeze
+echo "======================================================================"
+
+MARKER_TEXT="Project: $ZUUL_PROJECT Ref: $ZUUL_REFNAME Build: $ZUUL_UUID"
+echo $MARKER_TEXT > deploy-guide/build/html/.root-marker
+
+if [ -z "$ZUUL_REFNAME" ]; then
+    TARGET=""
+    # Leave documents where they are
+elif [ "$ZUUL_REFNAME" == "master" ] ; then
+    TARGET=draft
+elif echo $ZUUL_REFNAME | grep stable/ >/dev/null ; then
+    # Put stable release changes in dir named after stable release under the
+    # build dir. When Jenkins copies these files they will be accessible under
+    # the developer docs root using the stable release's name.
+    TARGET=$(echo $ZUUL_REFNAME | sed 's/stable.//')
+else
+    # Put other branch changes in dir named after branch under the
+    # build dir. When Jenkins copies these files they will be
+    # accessible under the developer docs root using the branch name.
+    # EG: feature/foo or milestone-proposed
+    TARGET=$ZUUL_REFNAME
+fi
+
+if [ ! -z $TARGET ] ; then
+    # Move the docs into subdir based on branch
+    TOP=`dirname $TARGET`
+    mv deploy-guide/build/html deploy-guide/build/tmp
+    mkdir -p deploy-guide/build/html/$TOP
+    mv deploy-guide/build/tmp deploy-guide/build/html/$TARGET
+fi
+
+exit $result
