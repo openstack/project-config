openstack/project-config

commit 2cd816937163c647936be5314ec4dc1719b05dd8

scripts: jenkins-sudo-grep: Look at /var/log/messages for sudo logs
The rsyslog package on openSUSE uses /var/log/messages for the sudo logs
so add this file as an alternative to the auth.log and secure ones.

Change-Id: I1af5a7a56590935585ac9684d90c2e7c90f09975

jenkins/scripts/jenkins-sudo-grep.sh
@@ -16,7 +16,7 @@
 # under the License.
 
 # Find out if jenkins has attempted to run any sudo commands by checking
-# the auth.log or secure log files before and after a test run.
+# the auth.log or secure log or messages files before and after a test run.
 
 PATTERN="sudo.*jenkins.*:.*\(incorrect password attempts\|command not allowed\)"
 if [ -f /var/log/auth.log ]; then
@@ -25,8 +25,11 @@ if [ -f /var/log/auth.log ]; then
 elif [ -f /var/log/secure ]; then
     OLDLOGFILE=$( ls /var/log/secure-* | sort | tail -n1 )
     LOGFILE=/var/log/secure
+elif [ -f /var/log/messages ]; then
+    OLDLOGFILE=$( ls /var/log/messages-* | sort | tail -n1 )
+    LOGFILE=/var/log/messages.log
 else
-    echo "*** Could not find auth.log/secure log for sudo tracing"
+    echo "*** Could not find auth.log/secure/messages log for sudo tracing"
     exit 1
 fi
 

commit 0b4140565dae941effb661b284349aac5821a940

jenkins: scripts: jenkins-sudo-grep: Drop extension on /var/log/messages
Commit 2cd816937163c ("scripts: jenkins-sudo-grep: Look at
/var/log/messages for sudo logs") added support for querying the
/var/log/messages file to detect sudo usage. However, there was a typo
and the file that was used was 'messages.log' instead of 'messages'
which is wrong.

Fixes: 2cd816937163c ("scripts: jenkins-sudo-grep: Look at /var/log/messages for sudo logs")
Change-Id: Iea27fe4853dc44221e7790268e0e6860547dfa6b

jenkins/scripts/jenkins-sudo-grep.sh
@@ -27,7 +27,7 @@ elif [ -f /var/log/secure ]; then
     LOGFILE=/var/log/secure
 elif [ -f /var/log/messages ]; then
     OLDLOGFILE=$( ls /var/log/messages-* | sort | tail -n1 )
-    LOGFILE=/var/log/messages.log
+    LOGFILE=/var/log/messages
 else
     echo "*** Could not find auth.log/secure/messages log for sudo tracing"
     exit 1