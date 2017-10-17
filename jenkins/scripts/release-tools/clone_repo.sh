97aaa1e3e5e0c0e359dea32e8d3d54ce5d90f985
Doug Hellmann <doug@doughellmann.com>
2017-10-17 17:01:12 -0400

    import the clone_repo.sh script from the releases repo
    Under zuulv3 the behavior has changed significantly to now require
    specifying which repositories need to be cloned for each job, and
    zuul-cloner only copies the repositories that are already
    available. As soon as zuulv3 is released, our jobs relying on the old
    fallback behavior of checking out from a cache when present but using
    the upstream server in other situations will fail.
    
    The new script clone_repo.sh handles the cases we have in the
    tag-releases job. It is a copy of
    openstack/releases/tools/clone_repo.sh from
    Ic559d27881a9a89fb0dcb5295a4d7ed7c578112f. The duplication is expected
    to be temporary because we want to move the tag-releases job
    definition into the releases repository after the milestone.
    
    The other changes update the shared function "clone_repo" to call the
    script rather than zuul-cloner. This approach was taken to make it
    clear that clone_repo.sh is a copy of the original script. The step to
    update the list of tags in the local repo is redundant, so that is
    also removed.
    
    Change-Id: I294d7ec37a6ba570065b965dcb461bc020668ce4
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ jenkins/scripts/release-tools/clone_repo.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# This script is used to clone a repository, possibly starting from a
+# local cache, and then update from the global remote. It replaces v2
+# of zuul-cloner, and is a bash script instead of being included in
+# openstack_releases/gitutils.py because we use it from jobs that
+# cannot run pip or tox for security reasons.
+#
+
+BINDIR=$(dirname $0)
+
+function print_help {
+    cat <<EOF
+USAGE:
+
+    clone_repo.sh -h
+
+    clone_repo.sh [--workspace WORK_DIR] [--cache-dir CACHE]
+                  [--branch BRANCH] [--ref REF]
+                  [--upstream URL] repo-name
+
+Arguments:
+
+  repo-name -- The full repository name, such as
+              "openstack/oslo.config". This name is also used as the
+              output directory.
+
+Options:
+
+  -h -- Print help.
+
+  --workspace -- The name of the parent directory where the cloned
+                 repo should be put.
+
+  --cache-dir -- A location where a local copy of a repo exists and
+                 can be used to seed the clone, which will still be
+                 updated from the remote. Defaults to $ZUUL_CACHE_DIR
+                 or /opt/git.
+
+  --branch -- The branch to check out. Defaults to "master".
+
+  --ref -- The git reference to check out. Defaults to HEAD.
+
+  --upstream -- The upstream server URL, without the git repo
+                part. Defaults to git://git.openstack.org
+
+EOF
+}
+
+# Defaults
+WORKSPACE="."
+CACHE_DIR="${ZUUL_CACHE_DIR:-/opt/git}"
+BRANCH="master"
+REF=""
+UPSTREAM="git://git.openstack.org"
+
+OPTS=`getopt -o h --long branch:,cache-dir:,ref:,upstream:,workspace: -n $0 -- "$@"`
+if [ $? != 0 ] ; then
+    echo "Failed parsing options." >&2
+    print_help
+    exit 1
+fi
+eval set -- "$OPTS"
+while true; do
+    case "$1" in
+        -h)
+            print_help
+            exit 0
+            ;;
+        --branch)
+            BRANCH="$2"
+            shift
+            shift
+            ;;
+        --cache-dir)
+            CACHE_DIR="$2"
+            shift
+            shift
+            ;;
+        --ref)
+            REF="$2"
+            shift
+            shift
+            ;;
+        --upstream)
+            UPSTREAM="$2"
+            shift
+            shift
+            ;;
+        --workspace)
+            WORKSPACE="$2"
+            shift
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+REPO="$1"
+shift
+
+if [ -z "$REPO" ]; then
+    print_help
+    echo "ERROR: No repository given."
+    exit 1
+fi
+
+if [ ! -d "$WORKSPACE" ]; then
+    echo "ERROR: Workspace $WORKSPACE does not exist."
+    exit 1
+fi
+
+set -x
+set -e
+
+cache_remote="$CACHE_DIR/$REPO"
+if [ ! -d "$cache_remote" ]; then
+    echo "WARNING: Cache directory $cache_remote does not exist, ignoring."
+    cache_remote=""
+fi
+
+upstream_remote="$UPSTREAM/$REPO"
+local_dir="$WORKSPACE/$REPO"
+
+# Clone the repository.
+if [ -d $local_dir ]; then
+    echo "Already have a local copy of $REPO in $local_dir"
+
+elif [ ! -z "$cache_remote" ]; then
+    # Clone from the cache then update the origin remote to point
+    # upstream so we can pull down more recent changes.
+    (cd $WORKSPACE &&
+            git clone $cache_remote $REPO &&
+            cd $REPO &&
+            git remote set-url origin "$upstream_remote"
+    )
+
+else
+    (cd $WORKSPACE && git clone $upstream_remote $REPO)
+fi
+
+# Make sure it is up to date compared to the upstream remote.
+(cd $local_dir &&
+        git remote update &&
+        git fetch origin --tags
+)
+
+if [ ! -z "$REF" ]; then
+    # Check out the specified reference.
+    (cd $local_dir && git checkout "$REF")
+else
+    # Check out the expected branch (master is the default, but if the
+    # directory already existed we might have checked out something else
+    # before so just do it again).
+    (cd $local_dir &&
+            git checkout $BRANCH &&
+            git pull --ff-only)
+fi
