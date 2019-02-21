c8582e4d26237b26076ec9598e55b88d22bb2b58
Ian Wienand <iwienand@redhat.com>
2019-02-21 14:33:22 +1100

    Add nodepool-dib dashboard
    This is a dashboard to track nodepool dib status.  We generate it
    client side as it seems easier and leads to a suitable layout.
    
    Depends-On: https://review.openstack.org/638288
    Change-Id: I5dee06347c0cd4faaad7f1800416b704eb10d117

---
+++ grafana/create-nodepool-dib.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Create the nodepool-dib.yaml dashboard with nodepool dib build
+# status.
+
+output_file=nodepool-dib.yaml
+
+function create {
+    local title="$1"
+    local key="$2"
+
+    sed -e "s/%TITLE%/${title}/; " \
+        -e "s/%KEY%/${key}/" \
+        nodepool-dib.image.template >> ${output_file}
+}
+
+cp nodepool-dib.base.template nodepool-dib.yaml
+create "Ubuntu Bionic" "ubuntu-bionic"
+create "Ubuntu Xenial" "ubuntu-xenial"
+create "Ubuntu Trusty" "ubuntu-trusty"
+create "Centos 7" "centos-7"
+create "Fedora 29" "fedora-29"
+create "Debian Stretch" "debian-stretch"
+create "Gentoo" "gentoo-17-0-systemd"
+create "openSUSE 15.0" "opensuse-150"
+create "openSUSE 42.3" "opensuse-423"
+create "Ubuntu Bionic arm64" "ubuntu-bionic-arm64"
+create "Ubuntu Xenial arm64" "ubuntu-xenial-arm64"
+
