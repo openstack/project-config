800ea0aa056efab44ad4ef1c7bfb6973d06b4ffc
Andreas Jaeger <aj@suse.com>
2019-09-14 16:55:43 +0200

    Switch project-config to promote jobs [3]: Update building
    For using opendev-tox-docs, jobs need to publish to a "html"
    subdirectory, update the docs-site and specs jobs.
    
    Update .gitignore for this.
    
    Update python files to use yaml.safe_load to avoid a warning.
    
    Remove unused docs environment from tox.ini.
    
    Depends-On: https://review.opendev.org/682213
    Change-Id: I8d7032a6e836ec26f347d16abd600514089a677b

--- specs/generate_specs_site.py
+++ specs/generate_specs_site.py
@@ -44,7 +44,7 @@ parser.add_argument(
 args = parser.parse_args()
 
 print('Reading project data from %r' % args.infile)
-infile = yaml.load(open(args.infile, 'r'))
+infile = yaml.safe_load(open(args.infile, 'r'))
 template_path = os.path.dirname(args.infile)
 
 template_context = {
@@ -53,7 +53,7 @@ template_context = {
     'all': infile['projects'] + infile['programs'],
 }
 
-outdir = os.path.join(template_path, 'output')
+outdir = os.path.join(template_path, 'html')
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
