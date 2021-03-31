5491ddc4cb7a16f9280f69ec035bfce89c97fce1
Jeremy Stanley <fungi@yuggoth.org>
2021-03-31 01:42:13 +0000

    Clean up OpenEdge configuration
    The OpenEdge cloud has been offline for five months, initially
    disabled in I4e46c782a63279d9c18ff4ba2944c15b3027114b, so go ahead
    and clean up lingering references. If it is restored later, this can
    be reverted fairly easily.
    
    Change-Id: I935d61b7101e221243a2d815b174b9b5a0eadb8c

--- grafana/create-nodepool.sh
+++ grafana/create-nodepool.sh
@@ -25,5 +25,4 @@ create Limestone 'limestone-*' nodepool-limestone.yaml
 create Linaro 'linaro-*' nodepool-linaro.yaml
 create OVH 'ovh-*' nodepool-ovh.yaml
 create Vexxhost 'vexxhost-*' nodepool-vexxhost.yaml
-create OpenEdge 'openedge-*' nodepool-openedge.yaml
 create Airship-Citycloud 'airship-*' nodepool-airship-citycloud.yaml
