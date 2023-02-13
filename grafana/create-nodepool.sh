7c81cf6eda90d722a26ecc0197a749f5d1f31b60
Jeremy Stanley <fungi@yuggoth.org>
2023-02-13 00:02:04 +0000

    Farewell limestone
    The mirror in our Limestone Networks donor environment is now
    unreachable, but we ceased using this region years ago due to
    persistent networking trouble and the admin hasn't been around for
    roughly as long, so it's probably time to go ahead and say goodbye
    to it.
    
    In preparation for cleanup of credentials in system-config, first
    remove configuration here except leave the nodepool provider with an
    empty diskimages list so that it will have a chance to pick up after
    itself.
    
    Change-Id: I504682884a1439fac84d514880757c2cd041ada6

--- grafana/create-nodepool.sh
+++ grafana/create-nodepool.sh
@@ -20,7 +20,6 @@ function create {
 }
 
 create Rackspace 'rax-*' nodepool-rax.yaml
-create Limestone 'limestone-*' nodepool-limestone.yaml
 create Linaro 'linaro-*' nodepool-linaro.yaml
 create OVH 'ovh-*' nodepool-ovh.yaml
 create Vexxhost 'vexxhost-*' nodepool-vexxhost.yaml
