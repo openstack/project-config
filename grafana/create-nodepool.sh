d709e82f709f83f21beb2ab851797357048cd2cf
James E. Blair <jim@acmegating.com>
2025-05-08 11:12:38 -0700

    Add zuul-launcher dashboards
    This adds dashboards for the newly-available zuul-launcher stats.
    
    Change-Id: I35174a2af2d57f12a20ba88e4324d84648931bc9
    Depends-On: https://review.opendev.org/c/opendev/grafyaml/+/949248

--- grafana/create-nodepool.sh
+++ grafana/create-nodepool.sh
@@ -25,3 +25,21 @@ create OVH 'ovh-*' nodepool-ovh.yaml
 create Vexxhost 'vexxhost-*' nodepool-vexxhost.yaml
 create OSUOSL 'osuosl-*' nodepool-osuosl.yaml
 create OpenMetal 'openmetal-*' nodepool-openmetal.yaml
+
+function create_zuul {
+    local provider="$1"
+    local stat_list="$2"
+    local output_file="$3"
+
+    sed -e "s/%PROVIDER%/${provider}/; " \
+        -e "s/%STAT_LIST%/${stat_list}/" \
+        -e "s/%OUTPUT_FILE%/${output_file}/" \
+        zuul-launcher.template > ${output_file}
+}
+
+create_zuul Rackspace 'rax' zuul-launcher-rax.yaml
+create_zuul Rackspace-Flex 'raxflex' zuul-launcher-raxflex.yaml
+create_zuul OVH 'ovh' zuul-launcher-ovh.yaml
+create_zuul Vexxhost 'vexxhost' zuul-launcher-vexxhost.yaml
+create_zuul OSUOSL 'osuosl' zuul-launcher-osuosl.yaml
+create_zuul OpenMetal 'openmetal' zuul-launcher-openmetal.yaml
